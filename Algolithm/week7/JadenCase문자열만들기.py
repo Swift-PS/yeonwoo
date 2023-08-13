45e22d93abef13c069bd2f5c585eecdf4d179173
yeonwoo <y8551472@gmail.com>
2023-08-13 21:03:23 +0900

    정확도 94_다시 풀어야함

--- Algolithm/week7/JadenCase문자열만들기.py
+++ Algolithm/week7/JadenCase문자열만들기.py
@@ -1,14 +1,13 @@
 def solution(s):
     answer = ''
-    #1. 문자열을 공백기준으로 split : split함수를 쓰면 그냥 리스트로 출력됨
-    s = s.split()
-    
-    
-    #2. wordArr에 word[0]가 int형이 아니라면 무조건 대문자로 바꿔서 wordArr에 담기,아니면 return   
+    s = s.split(" ")
     for i in range(len(s)) : 
-        if not s[i][0].isdecimal(): 
-            s[i] = s[i][0].upper() + s[i][1:].lower()
-    answer = ' '.join(s)
+        if s[i] == "" : 
+            answer += " "
+            continue ; 
+            
+        if not s[i][0].isdecimal() : #첫 글자가 숫자가 아니고 공백이 아니라면 
+            s[i] = s[i].capitalize()
+
+    answer = " ".join(s)
     return answer
-s = "3people unFollowed me"            
-solution(s)
